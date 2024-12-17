f49bd7a4cac5e6042256ec5279ac7aaf3f41c3e7
td-29th02 <tuandung290204@gmail.com>
2024-12-17 20:24:47 +0700

    Baithuchanh3

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+Route::get('/', [TaskController::class, 'index']);
+
+Route::resource('tasks', TaskController::class);
